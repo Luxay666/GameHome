52268571fabeb4fc7f84ee430a4e489959b79706
Luxay666 <romleg6249@gmail.com>
2026-04-22 22:13:12 +0200

    click sys

--- GameHome/GameHome/Game.cpp
+++ GameHome/GameHome/Game.cpp
@@ -1,17 +1,43 @@
 #include "Game.h"
+#include <iostream>
 
 Game::Game(sf::RenderWindow& window, int _windowWidth, int _windowHeight)
     : window(window)
     , windowWidth(_windowWidth)
     , windowHeight(_windowHeight)
 {
-
+    clickBar.setFillColor(sf::Color::Green);
+    clickBar.setPosition({ 10.f, 10.f });
+    clickBar.setSize({ 0.f, 20.f });
 }
 
 void Game::handleEvent(const std::optional<sf::Event>& event)
 {
     if (state == GameState::Menu)
+    {
         menu.handleEvent(event, window);
+    }
+    else if (state == GameState::Playing)
+    {
+        if (!event) return;
+        if (const auto* e = event->getIf<sf::Event::MouseButtonPressed>())
+        {
+            if (e->button == sf::Mouse::Button::Left)
+            {
+                clickCount++;
+                float width = std::min(clickCount * 10.f, 500.f);
+                clickBar.setSize({ width, 20.f });
+                std::cout << "Clics: " << clickCount << std::endl;
+            }
+            if (clickCount >= 50)
+            {
+                std::cout << "You win!" << std::endl;
+                state = GameState::Menu;
+                clickCount = 0;
+                clickBar.setSize({ 0.f, 20.f });
+			}
+        }
+    }
 }
 
 void Game::update()
@@ -22,7 +48,11 @@ void Game::update()
 
 void Game::draw()
 {
-    std::cout << "state: " << (int)state << std::endl;
     if (state == GameState::Menu)
         menu.draw(window);
+    else
+    {
+        // Fond noir + barre verte qui grandit
+        window.draw(clickBar);
+    }
 }
--- GameHome/GameHome/Game.h
+++ GameHome/GameHome/Game.h
@@ -2,24 +2,24 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
 #include "Menu.h"
-#include <iostream>
 
 enum class GameState { Menu, Playing };
 
 class Game
 {
+public:
+    Game(sf::RenderWindow& _window, int _windowWidth, int _windowHeight);
+    ~Game() = default;
+    void handleEvent(const std::optional<sf::Event>& event);
+    void update();
+    void draw();
+
 private:
     sf::RenderWindow& window;
     int windowWidth;
     int windowHeight;
     Menu menu;
     GameState state = GameState::Menu;
-    
-public:
-    Game(sf::RenderWindow& _window, int _windowWidth, int _windowHeight);
-    ~Game() = default;
-
-    void handleEvent(const std::optional<sf::Event>& event);
-    void update();
-    void draw();
+    int clickCount = 0;
+    sf::RectangleShape clickBar;
 };
